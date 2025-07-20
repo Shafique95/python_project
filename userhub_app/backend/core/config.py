a62097844ba38ebd9ad309050d825965ae0a6d3b
Shafiqul Islam <www.shafiquebd@gmail.com>
2025-07-20 11:09:27 +0600

    run on macos

--- userhub_app/backend/core/config.py
+++ userhub_app/backend/core/config.py
@@ -7,5 +7,7 @@ class Config:
     """অ্যাপ কনফিগারেশন, .env থেকে লোড হয়"""
     DATABASE_URL: str = os.getenv(
         "DATABASE_URL",
-        "mysql+aiomysql://root:root@localhost:3306/userhub_db"
+        # "mysql+aiomysql://root:root@localhost:3306/userhub_db", // my home ps db config
+        "mysql+aiomysql://root:root1234@localhost:3306/userhub_db"#// my office ps db config
+
     ) 
